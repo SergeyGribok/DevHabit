e1a23f631c9fb8ccc0935162142d54eccba75bda
Serhii Hrybok <sergejgribok@gmail.com>
2025-07-24 19:43:46 +0300

    Added habits controller with two GET methods.

--- src/DevHabit.Api/Program.cs
+++ src/DevHabit.Api/Program.cs
@@ -10,7 +10,12 @@ using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+})
+.AddXmlSerializerFormatters();
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
